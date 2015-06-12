80841c7ea0d450d62b2f96ceefd0f9d16f45ac73
ngcl <reckhhh@sina.com>
2015-06-12 23:02:36 +0800

    226_invertTree

---
+++ Python/226_InvertBinaryTree_Easy.py
@@ -0,0 +1,33 @@
+'''
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Tags: Tree
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {TreeNode}
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
